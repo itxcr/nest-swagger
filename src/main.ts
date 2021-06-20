b4a63b0c218147d23e4098f969afcca5cd62689b
xcr <itxcr@protonmail.com>
2021-06-21 01:59:53 +0800

    nest-swagger

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  const options = new DocumentBuilder()
+    .setTitle('博客API')
+    .setDescription('测试用')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options)
+  SwaggerModule.setup('api', app, document)
+
+  await app.listen(3000);
+}
+
+bootstrap();
